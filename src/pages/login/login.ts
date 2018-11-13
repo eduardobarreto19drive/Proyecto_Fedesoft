a0287e80a26595d6e1cb62bbd1e77594a5f0233c
DESKTOP-SMPGBH8\eduardo Santamaria <eduardobarreto19@gmail.com>
2018-11-12 20:19:46 -0500

    Modulo login y olvido contraseña Terminados - Luis Eduardo

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
-import { AuthProvider } from '../../providers/auth/auth';
+import { IonicPage, NavController,LoadingController, Loading, AlertController} from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+import { Observable } from 'rxjs/Observable';
+
 
 /**
  * Generated class for the LoginPage page.
@@ -15,50 +19,61 @@ import { AuthProvider } from '../../providers/auth/auth';
   templateUrl: 'login.html',
 })
 export class LoginPage {
-  user = { email: '', password: '' };
+  myForm: FormGroup;
+  user: Observable<firebase.User>;
+  public loading:Loading;
+
   constructor(
     public navCtrl: NavController,
-    public navParams: NavParams,
-    public auth: AuthProvider,
-    public alertCtrl: AlertController
+    public formBuilder: FormBuilder,
+    public afAuth: AngularFireAuth,
+    public alertCtrl: AlertController,
+    public loadingCtrl: LoadingController
   ) {
+    this.myForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+    this.user = afAuth.authState;
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
-  }
+  loginUser(){
 
-  signin() {
-    this.auth.registerUser(this.user.email, this.user.password)
-      .then((user) => {
-        // El usuario se ha creado correctamente
-      })
-      .catch(err => {
+    console.log("Email:" + this.myForm.value.email);
+    console.log("Password:" + this.myForm.value.password);
+   
+
+    this.afAuth.auth.signInWithEmailAndPassword(this.myForm.value.email, this.myForm.value.password).then(() => {
+      console.log("User logging");
+      //this.navCtrl.setRoot("HomePage");
+    }, (err) => {
+      this.loading.dismiss().then( () => {
         let alert = this.alertCtrl.create({
-          title: 'Error',
-          subTitle: err.message,
-          buttons: ['Aceptar']
+          message: err.message,
+          buttons: [
+            {
+              text: "Ok",
+              role: 'cancel'
+            }
+          ]
         });
         alert.present();
-      })
+      });
+    });
 
+    this.loading = this.loadingCtrl.create({
+      dismissOnPageChange: true,
+    });
+    this.loading.present();
+  }
+  
+
+  goToSignup(){
+    this.navCtrl.push('SignupPage');
   }
 
-  login() {
-    this.auth.loginUser(this.user.email, this.user.password).then((user) => {
-    }
-    )
-      .catch(err => {
-        let alert = this.alertCtrl.create({
-          title: 'Error',
-          subTitle: err.message,
-          buttons: ['Aceptar']
-        });
-        alert.present();
-      })
+  goToResetPassword(){
+    this.navCtrl.push('ResetPasswordPage');
   }
 
-  cerrarSesion() {
-    this.auth.logout();
-  }
 }
